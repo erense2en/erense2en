f6e77107b2b6e71789a5624fcb26e3d90a3cc253
Eren Sezen <erensezen1337@gmail.com>
2026-03-24 01:45:46 +0300

    Update dayjs.ts

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -4,7 +4,9 @@ import isBetween from 'dayjs/plugin/isBetween'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import timezone from 'dayjs/plugin/timezone'
+import 'dayjs/locale/tr'
 
+dayjs.locale('tr')
 dayjs.extend(utc)
 dayjs.extend(isBetween)
 dayjs.extend(advancedFormat)

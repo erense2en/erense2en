611ad855e6da348360722d7e2d53ea7b0468f636
Eren Sezen <erensezen1337@gmail.com>
2026-03-24 18:03:31 +0300

    Update links.tsx

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { ProjectData } from 'data/links'
 import { ProjectCard } from 'components/ProjectCard'
 
-const Projects = () => {
+const Links = () => {
   return (
     <MainLayout
       title="Links"
